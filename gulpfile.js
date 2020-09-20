3454fcef4bbe5cf4492d126c93deeacf6bc8e16e
Alzintani <hassan_abubaker@yahoo.com>
2020-09-20 05:19:36 +0200

    Updating gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
 // including plugins
-const gulp    = require('gulp')
-const babel   = require("gulp-babel")
-const notify  = require("gulp-notify")
-const header  = require('gulp-header')
-const include = require("gulp-include")
-const minify  = require('gulp-minify')
-const sass    = require('gulp-sass')
-const rename  = require('gulp-rename')
-const pug     = require('gulp-pug')
-const parse   = require('comment-parser')
+const gulp    = require('gulp');
+const babel   = require("gulp-babel");
+const notify  = require("gulp-notify");
+const header  = require('gulp-header');
+const include = require("gulp-include");
+const minify  = require('gulp-minify');
+const sass    = require('gulp-sass');
+const rename  = require('gulp-rename');
+const pug     = require('gulp-pug');
+const parse   = require('comment-parser');
 
 const callNotify = ( err, mesg ) => {
-  let urgency = err ? 'critical' : 'low'
-  let msg = err ? String(err).substring(0,200)+"..." : ''
+  let urgency = err ? 'critical' : 'low';
+  let msg = err ? String(err).substring(0,200)+"..." : '';
 
-  console.log(mesg)
+  console.log(mesg);
 
   return gulp.src("./").pipe(notify({
     title: 'SuperEvents',
@@ -22,11 +22,11 @@ const callNotify = ( err, mesg ) => {
     // icon: '',
     urgency: urgency,
     'expire-ime': 1
-  }))
+  }));
 }
 
 // using data from package.json
-var pkg = require('./package.json')
+var pkg = require('./package.json');
 var banner = ['/*!',
   ' * SuperEvents v<%= pkg.version %> (<%= pkg.homepage %>)',
   ' * Copyright 2018 SuperEvents',
@@ -39,46 +39,46 @@ var banner = ['/*!',
   ' */',
   '',
   '',
-].join('\n')
+].join('\n');
 
 const apiGenerate = async () => {
 
-  const path = './src/SuperEvents'
+  const path = './src/SuperEvents';
 
-  let events = []
-  let event
+  let events = [];
+  let event;
 
   const streamToString = (stream) => {
-    const chunks = []
+    const chunks = [];
     return new Promise((resolve, reject) => {
-      stream.on('data', chunk => chunks.push(chunk) )
-      stream.on('error', reject )
-      stream.on('end', () => resolve(chunks))
-    })
+      stream.on('data', chunk => chunks.push(chunk) );
+      stream.on('error', reject );
+      stream.on('end', () => resolve(chunks));
+    });
   }
 
   const callback = ( err, data ) => {
     if (err) {
-      console.log(err, data)
-      return false
+      console.log(err, data);
+      return false;
     }
-    return data
+    return data;
   }
 
   for( let val of ['hover', 'click', 'mousemove', 'scroll'] ) {
     event = await streamToString(
       parse.file( `${path}/events/${val}.js`, callback)
-    )
-    events = [ event, ...events ]
+    );
+    events = [ event, ...events ];
   }
 
   const actions = await streamToString(
     parse.file( `${path}/actions/actions.js`, callback)
-  )
+  );
 
   const easing = await streamToString(
     parse.file( `${path}/actions/easing.js`, callback)
-  )
+  );
 
   return gulp.src( './src/pug/**.pug' )
     .pipe( pug({
@@ -90,25 +90,25 @@ const apiGenerate = async () => {
       }
     }) )
     .on('error', (err) => console.log(err))
-    .pipe(gulp.dest('./docs/'))
+    .pipe(gulp.dest('./docs/'));
 }
 
 const runServer = () => {
-  const express = require('express')
-  const path = require('path')
-  const port = 3001
+  const express = require('express');
+  const path = require('path');
+  const port = 3001;
 
-  const app = express()
+  const app = express();
 
-  app.use( express.static(path.join(__dirname, 'docs')) )
-  app.use( '/dist/', express.static(__dirname + '/dist/'))
-  app.use( '/logo.png', express.static(__dirname + '/logo.png'))
-  app.use( '/favicon.ico', express.static(__dirname + '/favicon.ico'))
+  app.use( express.static(path.join(__dirname, 'docs')) );
+  app.use( '/dist/', express.static(__dirname + '/dist/'));
+  app.use( '/logo.png', express.static(__dirname + '/logo.png'));
+  app.use( '/favicon.ico', express.static(__dirname + '/favicon.ico'));
 
   app.listen(port, function () {
-    console.log(`> SuperEvents listening on port: ${port}`)
-    console.log(`> Now. visit http://localhost:${port}`)
-  })
+    console.log(`> SuperEvents listening on port: ${port}`);
+    console.log(`> Now. visit http://localhost:${port}`);
+  });
 }
 
 gulp.task('compile-es6', function (cb) {
@@ -118,18 +118,17 @@ gulp.task('compile-es6', function (cb) {
     .pipe( babel() ).on('error', callNotify) // compile JS code to es5
     .pipe(header(banner, {pkg: pkg})).on('error', callNotify) // add file header
     .pipe(minify({ ext:{ min:'.min.js' } }))
-    .pipe(gulp.dest('./dist/')) &&
+    .pipe(gulp.dest('./dist/'))
+  && apiGenerate();
 
-  apiGenerate()
-
-})
+});
 
 gulp.task('js', function (cb) {
   return gulp.src('./src/js/javascript.js')
     .pipe( babel() ).on('error', callNotify) // compile JS code to es5
     .pipe(minify({ ext:{ min:'.min.js' } }))
-    .pipe(gulp.dest('./docs/assets/js/'))
-})
+    .pipe(gulp.dest('./docs/assets/js/'));
+});
 
 gulp.task('sass', function () {
   return gulp.src('./src/scss/style.scss')
@@ -138,46 +137,46 @@ gulp.task('sass', function () {
     }).on('error', callNotify))
     .pipe(rename({
       suffix: '.min'
-    })).pipe(gulp.dest('./docs/assets/css/'))
-})
+    })).pipe(gulp.dest('./docs/assets/css/'));
+});
 
 gulp.task('pug', function () {
-  return apiGenerate()
-})
+  return apiGenerate();
+});
 
 gulp.task('docs', async () => {
-  return apiGenerate()
-})
+  return apiGenerate();
+});
 
 // default task
 gulp.task('watch', function() {
   gulp.watch(
     ['./src/SuperEvents/**/*.js'], gulp.series('compile-es6', 'copy-to-docs', 'notify')
-  )
+  );
 
   gulp.watch(
     ['./src/js/javascript.js'], gulp.series('js', 'notify')
-  )
+  );
 
   gulp.watch(
     ['./src/scss/**/*.scss'], gulp.series('sass', 'notify')
-  )
+  );
 
   gulp.watch(
     ['./src/pug/**/*.pug'], gulp.series('pug', 'notify')
-  )
+  );
 
-  runServer()
+  runServer();
 })
 
 // default task
 gulp.task('copy-to-docs', function() {
-  return  gulp.src(['dist/**.js']).pipe(gulp.dest('docs/assets/js'))
-})
+  return  gulp.src(['dist/**.js']).pipe(gulp.dest('docs/assets/js'));
+});
 
 // default task
 gulp.task('notify', function() {
-  return callNotify( null, 'SuperEvents is Compiled!!!!' )
-})
+  return callNotify( null, 'SuperEvents is Compiled!!!!' );
+});
 
-gulp.task('build', gulp.series( 'pug', 'sass', 'js', 'compile-es6' ))
+gulp.task('build', gulp.series( 'pug', 'sass', 'js', 'compile-es6' ));
